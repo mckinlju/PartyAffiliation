3903cc64f8ddb3294128aef1b236364ac8b21be7
mckinlju <mckinlju@mail.uc.edu>
2023-09-25 18:38:28 -0400

    Final Program

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,27 @@ public class Main
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
+        String menuChoice = "";
+        String menu = "R - Republican\tD - Democrat\tI - Independent\tO - Other\nChoose your affiliation: ";
+
+        System.out.print(menu);
+        menuChoice = in.nextLine();
+
+        if(menuChoice.equalsIgnoreCase("R"))
+        {
+            System.out.println("You get a Republican Elephant!");
+        }
+        else if(menuChoice.equalsIgnoreCase("D"))
+        {
+            System.out.println("You get a Democratic Donkey!");
+        }
+        else if(menuChoice.equalsIgnoreCase("I"))
+        {
+            System.out.println("You get an Independent Person!");
+        }
+        else if(menuChoice.equalsIgnoreCase("O"))
+        {
+            System.out.println("You are affiliate with another party!");
+        }
     }
 }
